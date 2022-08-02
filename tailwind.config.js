8cc9293f60b13c97a4c692704f6f106dc1789c60
revivaldefitech <89870916+revivaldefitech@users.noreply.github.com>
2022-08-02 01:30:49 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,13 +15,13 @@ module.exports = {
   daisyui: {
     themes: [{
       rvl: {
-        "primary": "#32D0AA",
+        "primary": "#FF0069",
         "secondary": "#0D0C2B",
-        "accent": "#37CDBE",
+        "accent": "#e6005f",
         "neutral": "#262641",
         "base-100": "#FFFFFF",
         "info": "#3ABFF8",
-        "success": "#36D399",
+        "success": "#cd0054",
         "warning": "#FBBD23",
         "error": "#F87272",
       }
